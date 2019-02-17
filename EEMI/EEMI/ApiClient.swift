392b3631b785512d580751973fe8e6f5a6116292
Jorge <jelizondolevy.s@gmail.com>
2019-02-17 14:01:01 -0600

    Adds cocoa pods and swiftlint rules

---
+++ EEMI/EEMI/ApiClient.swift
@@ -0,0 +1,9 @@
+//
+//  ApiClient.swift
+//  EEMI
+//
+//  Created by Jorge Elizondo on 2/17/19.
+//  Copyright © 2019 Io Labs. All rights reserved.
+//
+
+import Foundation
---
+++ EEMI/Pods/Target Support Files/Pods-EEMITests/Pods-EEMITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EEMITests : NSObject
+@end
+@implementation PodsDummy_Pods_EEMITests
+@end
