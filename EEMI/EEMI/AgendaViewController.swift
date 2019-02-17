943a835863f04ee103e6374dd16ebf3e9f8a11ba
Jorge <jelizondolevy.s@gmail.com>
2019-02-17 12:55:02 -0600

    Adds initial setup

---
+++ EEMI/EEMI/AgendaViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AgendaViewController.swift
+//  EEMI
+//
+//  Created by Jorge Elizondo on 2/17/19.
+//  Copyright © 2019 Io Labs. All rights reserved.
+//
+
+import UIKit
+
+class AgendaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
---
+++ EEMI/EEMI/PacientsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PacientsViewController.swift
+//  EEMI
+//
+//  Created by Jorge Elizondo on 2/17/19.
+//  Copyright © 2019 Io Labs. All rights reserved.
+//
+
+import UIKit
+
+class PacientsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
