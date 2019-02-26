806bc0bcd8d2d4dacc1267176ba5fb75cee3c21d
Oscar Elizondo <oscarmarcelo95@gmail.com>
2019-02-25 20:56:29 -0600

    merges appointment date and appointment time to a single date object

--- EEMI/EEMI/ApiClient.swift
+++ EEMI/EEMI/ApiClient.swift
@@ -9,7 +9,6 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
-import Cache
 
 enum Result<T> {
     case success(T)
--- EEMI/EEMI/Appointment.swift
+++ EEMI/EEMI/Appointment.swift
@@ -14,11 +14,26 @@ class Appointment {
     var patientLastName: String?
     var reason: String?
     var comments: String?
-    
+    var appointmentTime: Date?
+
     init(json: JSON) {
         self.patientName = json["patientName"].string
         self.patientLastName = json["patientLastName"].string
         self.comments = json["comments"].string
         self.reason = json["reason"].string
+        self.appointmentTime = join(json: json)
+    }
+
+    private
+    func join (json: JSON) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+
+        let date = json["dateAppointment"].string ?? "0000-00-00"
+        let time = json["timeAppointment"].string ?? "00:00:00"
+        var splitDate = date.split(separator: "T")
+
+        let joinedDates = splitDate[0] + "'T'" + time
+        return dateFormatter.date(from: joinedDates)!
     }
 }
