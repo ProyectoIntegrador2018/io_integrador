617c070d7ef2749b1b4dfaced7c3064f2c7b09c6
Oscar Elizondo <oscarmarcelo95@gmail.com>
2019-05-12 17:59:19 -0500

    refactors error messages to a class

--- EEMI/EEMI/ApiClient.swift
+++ EEMI/EEMI/ApiClient.swift
@@ -56,13 +56,9 @@ class ApiClient {
                 completion(.success(token))
                 
             case let .failure(error):
-                if error._code == NSURLErrorNotConnectedToInternet {
-                    completion(.error(("Inténtalo más tarde.", "No pudimos conectarnos a internet.")))
-                } else {
-                    let errorMessage = "Usuario o contraseña inválida."
-                    let errorTitle = "No se pudo iniciar sesión."
-                    completion(.error((errorMessage, errorTitle)))
-                }
+                let statusCode = response.response?.statusCode
+                let err = ErrorClient(error: error, statusCode: statusCode!, source: "getToken")
+                completion(.error(err.handleError()))
             }
         }
     }
@@ -94,13 +90,9 @@ class ApiClient {
                 completion(.success(appointments))
                 
             case let .failure(error):
-                if error._code == NSURLErrorNotConnectedToInternet {
-                    completion(.error(("Inténtalo más tarde.", "No pudimos conectarnos a internet.")))
-                } else if response.response?.statusCode == 401 {
-                    completion(.error(("Cierra la sesión y vuelve ingresar", "Tú sesión expiro")))
-                } else {
-                    completion(.error(("Inténtalo más tarde.", "No se pudieron obtener citas.")))
-                }
+                let statusCode = response.response?.statusCode
+                let err = ErrorClient(error: error, statusCode: statusCode!, source: "getAppointments")
+                completion(.error(err.handleError()))
             }
         }
     }
@@ -128,13 +120,9 @@ class ApiClient {
                 completion(.success(patients))
                 
             case let .failure(error):
-                if error._code == NSURLErrorNotConnectedToInternet {
-                    completion(.error(("Inténtalo más tarde.", "No pudimos conectarnos a internet.")))
-                } else if response.response?.statusCode == 401 {
-                    completion(.error(("Cierra la sesión y vuelve ingresar", "Tú sesión expiro")))
-                } else {
-                    completion(.error(("Inténtalo más tarde.", "No se pudieron obtener los pacientes.")))
-                }
+                let statusCode = response.response?.statusCode
+                let err = ErrorClient(error: error, statusCode: statusCode!, source: "getPatients")
+                completion(.error(err.handleError()))
             }
         }
     }
@@ -156,13 +144,9 @@ class ApiClient {
                 completion(.success(medicalRecord))
                 
             case let .failure(error):
-                if error._code == NSURLErrorNotConnectedToInternet {
-                    completion(.error(("Inténtalo más tarde.", "No pudimos conectarnos a internet.")))
-                } else if response.response?.statusCode == 401 {
-                    completion(.error(("Cierra la sesión y vuelve ingresar", "Tú sesión expiro")))
-                } else {
-                    completion(.error(("Inténtalo más tarde.", "No se pudo obtener el expediente médico.")))
-                }
+                let statusCode = response.response?.statusCode
+                let err = ErrorClient(error: error, statusCode: statusCode!, source: "getMedicalRecord")
+                completion(.error(err.handleError()))
             }
         }
     }
